b4e8a6bf8d2f4e91ef2c06ff5e142fbb4b017ac3
ManishWebtech163 <panchalmanish163@gmail.com>
2023-08-16 10:36:02 +0530

    css add

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+var swiper = new Swiper(".heroSectionLogosSlider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
