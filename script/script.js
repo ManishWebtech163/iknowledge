b78f942b60f2b28c075eb92bf850c2eff5e777b8
vishal007p <107975264+vishal007p@users.noreply.github.com>
2023-09-29 17:32:00 +0530

    my code

--- script/script.js
+++ script/script.js
@@ -7,3 +7,19 @@ var swiper = new Swiper(".heroSectionLogosSlider", {
         clickable: true,
     },
 });
+
+
+// --- side bar   js code ---
+
+
+// Get the sidebar and toggle button elements
+const sidebar = document.querySelector('.sidebar');
+const toggleButton = document.getElementById('toggleSidebar');
+
+// Function to toggle the sidebar
+function toggleSidebar() {
+    sidebar.classList.toggle('sidebar-open');
+}
+
+// Add click event listener to the toggle button
+toggleButton.addEventListener('click', toggleSidebar);
